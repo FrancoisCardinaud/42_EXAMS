53398adb4147ec7f1437f3831d6c312ce332881f
Francois Cardinaud <fcardina@e4r5p3.42roma.it>
2023-02-14 16:39:29 +0100

    14022023

---
+++ 42_Exams_Pasquale_Rossi copy/42-School-Exam-Rank-06/mini_serv.c
@@ -0,0 +1,79 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+
+typedef int bool;
+
+bool	check_arguments(int argc)
+{
+	return (argc > 1);
+}
+
+void	putstr_fd(char *str, int fd)
+{
+	write(fd, str, strlen(str));
+}
+
+void	arguments_error(void)
+{
+	putstr_fd("Wrong number of arguments\n", 2);
+	exit(1);
+}
+
+void	fatal_error(void)
+{
+	putstr_fd("Fatal error\n", 2);
+	exit(1);
+}
+
+int get_port(char *arg)
+{
+	int	port = atoi(arg);
+	return (port);
+}
+
+int	main(int argc, char **argv)
+{
+	int						port;
+	int						server_fd;
+	int						client_fd;
+	char					buf[1024];
+	struct sockaddr_in		addr;
+	socklen_t 				addr_len;
+	fd_set					fds;
+	
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr_len = sizeof(addr);
+
+	if (!check_arguments(argc))
+		arguments_error();
+	if ((port = get_port(argv[1])) == -1)
+		fatal_error();
+	printf("port = %d\n", port);
+	if (bind(server_fd, (struct sockaddr *)&addr, addr_len)== -1)
+		fatal_error();
+	if (listen(server_fd, 0) == -1)
+		fatal_error();
+	while (1)
+	{
+		if ((client_fd = accept(server_fd, (struct sockaddr *)&addr, &addr_len)) == -1)
+			fatal_error();
+		FD_ZERO(&fds);
+		FD_SET(client_fd, &fds);
+		select(client_fd + 1, &fds, NULL, NULL, NULL);
+		if (FD_ISSET(client_fd, &fds))
+		{
+			recv(client_fd, buf, 1024, 0);
+			printf("Recv: %s\n", buf);
+		}
+	}
+	return (0);
+}
---
+++ 42_Exams_Pasquale_Rossi copy/README.md
@@ -0,0 +1,51 @@
+# 42 Piscine Exams
+
+🖥️ [Piscine Exams](https://github.com/pasqualerossi/42-Piscine/tree/main/42%20Piscine%20Exam) 
+
+> Exam 00, 01, 02 and Final Piscine Exam
+
+<br>
+
+# 42 School Exams
+
+🖥️ [Exam Rank 02](https://github.com/pasqualerossi/42-School-Exam-Rank-02) 
+> 4 Piscine Questions
+
+<br>
+
+🖥️ [Exam Rank 03](https://github.com/pasqualerossi/42-School-Exam-Rank-03) 
+
+> 1 Question - either Get_Next_Line or Ft_Printf
+
+<br>
+
+🖥️ [Exam Rank 04](https://github.com/pasqualerossi/42-School-Exam-Rank-04) 
+
+> 1 Question - Microshell
+
+<br>
+
+🖥️ [Exam Rank 05](https://github.com/pasqualerossi/42-School-Exam-Rank-05) 
+
+> 3 C++ Questions
+
+<br>
+
+🖥️ [Exam Rank 06](https://github.com/pasqualerossi/42-School-Exam-Rank-06) 
+
+> 1 Question - Mini_Serv
+
+<br>
+
+## Total
+
+- 4 Piscine Exams
+- 5 School Exams
+
+> 9 Exams from the start of the Piscine to the end of the Inner Circle.
+
+<br>
+
+# Note
+
+if you can make any program or function shorter, but readable, please do let me know and I'll update it. 
